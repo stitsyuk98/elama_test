955d32b4318e6e70b942fce7515baef042b6a5e7
Nadejda <stitsyuk98@mail.ru>
2022-02-07 23:26:35 +0300

    the request is written taking into account all the requirements

--- airflow/sql/sales_sumary.sql
+++ airflow/sql/sales_sumary.sql
@@ -1,14 +1,15 @@
 CREATE MATERIALIZED VIEW IF NOT EXISTS sales_summary AS
-select user_id, sum(price)
+select us.email, sum(t.price)
 from transactions t
+join users us using(user_id)
 where exists (
     select user_id 
     from webinar w
     left join users u using(email)
     where date_registration > '2016-04-01' and t.user_id = u.user_id
         and w.email not in(
-                    select email
-                    from users u
-                    where date_registration < '2016-04-01'
-                ) )
-group by user_id;
+            select email
+            from users u
+            where date_registration < '2016-04-01'
+        ) )
+group by us.email;
