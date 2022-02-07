f120804a62d031c708f4d68ab69e0b3adeebd46e
Nadejda <stitsyuk98@mail.ru>
2022-02-07 23:05:37 +0300

    improved query

--- airflow/sql/sales_sumary.sql
+++ airflow/sql/sales_sumary.sql
@@ -3,15 +3,12 @@ select user_id, sum(price)
 from transactions t
 where exists (
     select user_id 
-    from (
-        select *
-        from webinar w
-        where email not in(
-            select email
-            from users u
-            where date_registration < '2016-04-01'
-        ) 
-    ) as tbl
-left join users u using(email)
-where date_registration > '2016-04-01' and t.user_id = u.user_id)
+    from webinar w
+    left join users u using(email)
+    where date_registration > '2016-04-01' and t.user_id = u.user_id
+        and w.email not in(
+                    select email
+                    from users u
+                    where date_registration < '2016-04-01'
+                ) )
 group by user_id;
